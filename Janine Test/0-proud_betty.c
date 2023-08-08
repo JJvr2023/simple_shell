854082f5e174d2769c4ea0f266f24fc6dc1f8019
JJvr2023 <129042030+JJvr2023@users.noreply.github.com>
2023-08-08 15:32:27 +0200

    Create 0-proud_betty.c

---
+++ Janine Test/0-proud_betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  char name[] = "Betty";
+  char description[] = "I am a proud shell";
+  char creator[] = "Alx";
+  char learning[] = "I am learning to be a great shell";
+
+  printf("My name is %s\n", name);
+  printf("I am a %s\n", description);
+  printf("I was created by %s\n", creator);
+  printf("I am learning to be a %s\n", learning);
+
+  return 0;
+}
