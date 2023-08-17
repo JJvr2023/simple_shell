4d1c192b8312d10cb68fc357b672a48597d915fd
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-08-17 04:27:59 -0700

    Amanda&Janine

---
+++ executable.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * executable - it execute commands.
+ * @args: handle errors.
+ *
+ * Return: (1) Success, (0) Failure.
+ */
+int executable(char **args)
+{
+	char *builtin_in_list[] = {"cd", "help", "env", "exit"};
+	int (*builtin_in_func[])(char **) = {&own_help, &own_cd, &own_env, &own_exist};
+	int index = 0;
+
+	if (args == NULL)
+	{
+	return -1;
+	}
+	while (index < sizeof(built_in_list)/ sizeof(char *))
+	{
+	if (!strcmp(args[0], built_in_list[index])
+			{
+				return (built_in_func[index](args);
+						}
+					}
+					return (new_process(args));
+					}
